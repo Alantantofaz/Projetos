e05cf2402ff29637332b33383185294491161b72
Alan Chagas <t.onni@hotmail.com>
2024-06-14 08:05:49 -0300

    add

--- main.py
+++ main.py
@@ -7,4 +7,16 @@ print(pyautogui.size())
 
 #Funções do mouse
 time.sleep(5)
-pyautogui.click()
+
+#clica em algum lugar
+
+# -> pyautogui.click()
+
+#Mover em algum lugar
+
+pyautogui.moveTo()
+#É possível colocar um tempo para realizar essa posição
+
+#tempo para executar comandos
+pyautogui.PAUSE = 1
+
